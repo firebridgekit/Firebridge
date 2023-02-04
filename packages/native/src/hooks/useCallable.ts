9f8684b61a2b7c7da4a11c530fc7f2f4a580e0ba
Mitch Butler <mitchellbutler@gmail.com>
2023-02-04 13:19:18 +0100

    [fix] uses native code for native useCallable

--- packages/native/src/hooks/useCallable.ts
+++ packages/native/src/hooks/useCallable.ts
@@ -1,19 +1,12 @@
-import { Functions, httpsCallable } from 'firebase/functions'
+import functions from '@react-native-firebase/functions'
 import { useFirebridge } from '../contexts'
 
-export const useCallable = <B = undefined, T = void>(
-  functionsInstance: Functions,
-  name: string,
-) => {
+export const makeCallable = <T, B = any>(name: string) => {
   const { log } = useFirebridge()
-  const callable = httpsCallable(functionsInstance, name)
+  const callable = functions().httpsCallable(name)
   return async (body?: B): Promise<T> => {
-    try {
-      const result = await callable({ ...(body as B) })
-      return result?.data as T
-    } catch (error) {
-      log.error(error)
-      throw error
-    }
+    log.debug(`[Cloud Callable]: ${name}`, { ...(body as B) })
+    const result = await callable({ ...(body as B) })
+    return result?.data
   }
 }
