9f1d9b8beee94235ba278cc3e2561fc424da9edc
Mitch Butler <mitchellbutler@gmail.com>
2023-02-04 18:03:26 +0100

    [feat] adds error descriptions

---
+++ packages/native/src/utils/index.ts
@@ -0,0 +1,20 @@
+const errorCodeMessages: { [key: string]: any } = {
+  'auth/invalid-email': {
+    email: 'Sorry, that email is invalid.',
+  },
+  'auth/user-disabled': {
+    email: 'Sorry, that account is disabled.',
+  },
+  'auth/user-not-found': {
+    email: "There isn't a user with that email.",
+  },
+  'auth/wrong-password': {
+    password: "You've entered an incorrect password.",
+  },
+  unknown: {
+    email: 'Sorry, something went wrong.',
+  },
+}
+
+export const describeErrorCode = (code: string) =>
+  errorCodeMessages[code] || errorCodeMessages.unknown
