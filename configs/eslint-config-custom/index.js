f4b48c42e621643c3af4b14cde5aaa00a781dba2
Mitchell Butler <mitchellbutler@gmail.com>
2025-08-08 07:31:58 -0400

    feat: adding lint check

--- configs/eslint-config-custom/index.js
+++ configs/eslint-config-custom/index.js
@@ -1,7 +1,13 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["turbo", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
   },
 };
