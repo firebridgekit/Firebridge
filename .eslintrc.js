1a76d58f5c15bcb8a07de92709b85a381eea9bc7
Mitchell Butler <mitchellbutler@gmail.com>
2025-08-08 07:18:36 -0400

    fix: fixing local packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ['@repo/eslint-config'],
+  extends: ['@configs/eslint-config'],
   settings: {},
 }
