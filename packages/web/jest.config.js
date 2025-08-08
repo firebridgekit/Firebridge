0ad590950e6fd3c841b47022262c33bf2ee17602
Mitchell Butler <mitchellbutler@gmail.com>
2025-08-08 06:59:37 -0400

    feat: adding testing

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapping: {
+  moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
   extensionsToTreatAsEsm: ['.ts', '.tsx']
