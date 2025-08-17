7016401092778a2ccf48db1de086a6ceba53a0c5
Mitchell Butler <mitchellbutler@gmail.com>
2025-08-17 08:49:41 -0400

    fix: revert to standard metadata for compat

--- packages/types/dist/index.d.ts
+++ packages/types/dist/index.d.ts
@@ -12,25 +12,21 @@ type SerializedFirestoreTimestamp = {
 /**
  * @typedef Metadata
  * @description Represents the metadata of a Firestore document.
- * @property {string} [status] - The status of the document.
  * @property {FirestoreTimestamp} timeCreated - The time when the document was created.
  * @property {FirestoreTimestamp} [timeUpdated] - The time when the document was last updated.
- * @property {FirestoreTimestamp} [timeDeleted] - The time when the document was deleted.
  */
 type EditorialMetadata<TS = FirestoreTimestamp> = {
-    status?: 'draft' | 'published' | 'archived' | 'deleted';
     timeCreated: TS;
     timeUpdated?: TS;
-    timeDeleted?: TS;
 };
 /**
  * @typedef WithEditorialMetadata
  * @template T - The type of the object.
  * @type {object}
- * @property {EditorialMetadata} _editorialMetadata - The metadata of the object.
+ * @property {EditorialMetadata} metadata - The metadata of the object.
  */
 type WithEditorialMetadata<T, TS = FirestoreTimestamp> = T & {
-    _editorialMetadata: EditorialMetadata<TS>;
+    metadata: EditorialMetadata<TS>;
 };
 
 /**
--- packages/types/src/EditorialMetadata.ts
+++ packages/types/src/EditorialMetadata.ts
@@ -3,24 +3,20 @@ import { FirestoreTimestamp } from './FirestoreTimestamp'
 /**
  * @typedef Metadata
  * @description Represents the metadata of a Firestore document.
- * @property {string} [status] - The status of the document.
  * @property {FirestoreTimestamp} timeCreated - The time when the document was created.
  * @property {FirestoreTimestamp} [timeUpdated] - The time when the document was last updated.
- * @property {FirestoreTimestamp} [timeDeleted] - The time when the document was deleted.
  */
 export type EditorialMetadata<TS = FirestoreTimestamp> = {
-  status?: 'draft' | 'published' | 'archived' | 'deleted'
   timeCreated: TS
   timeUpdated?: TS
-  timeDeleted?: TS
 }
 
 /**
  * @typedef WithEditorialMetadata
  * @template T - The type of the object.
  * @type {object}
- * @property {EditorialMetadata} _editorialMetadata - The metadata of the object.
+ * @property {EditorialMetadata} metadata - The metadata of the object.
  */
 export type WithEditorialMetadata<T, TS = FirestoreTimestamp> = T & {
-  _editorialMetadata: EditorialMetadata<TS>
+  metadata: EditorialMetadata<TS>
 }
